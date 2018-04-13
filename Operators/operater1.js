4c2023be31ebfbbdfe0a329d5fa028dbd9350892
Jackie <jacgut19@gmail.com>
2018-04-13 14:36:28 +0000

    Operator 1 and 2

---
+++ Operators/operater1.js
@@ -0,0 +1,11 @@
+// Add Two Numbers with JavaScript 
+var sum = 10 + 10; /*output: sum = 20 */
+
+// Subtract One Number from Another with JavaScript
+var difference = 45 - 33; /*output: difference = 12 */
+
+// Multiply Two Numbers with JavaScript
+var product = 8 * 10; /*output: product = 80 */
+
+// Divide One Number by Another with JavaScript
+var quotient = 66 / 33; /*output: quotient = 2 */
---
+++ Operators/operater2.js
@@ -0,0 +1,32 @@
+/* Increment a Number with JavaScript */ 
+var myVar = 87;
+myVar++; 
+// output: myVar = 88
+
+
+/* Decrement a Number with JavaScript */
+var myVar = 11;
+myVar--; 
+// output: myVar = 10
+
+
+/* Create Decimal Numbers with JavaScript */
+var ourDecimal = 5.7;
+var myDecimal = 2.8;
+// output: 2.8
+
+
+/* Multiply Two Decimals with JavaScript */
+var product = 2.0 * 2.5;
+// output: product = 5
+
+
+/* Divide one Decimal by Another with JavaScript */
+var quotient = 4.4 / 2.0;
+// output: quotient = 2.2
+
+/* Finding a Remainder in JavaScript */
+Math.floor (Math.random(11 % 3));
+var remainder = 2 % 11;
+// output: remainder = 2
+
