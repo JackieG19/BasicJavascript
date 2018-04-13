5505e1cdad6a578a03f55aaabec1d58be23fec33
Jackie <jacgut19@gmail.com>
2018-04-13 13:51:06 +0000

    first six problems solved

---
+++ Variables/var1.js
@@ -0,0 +1,39 @@
+//hello//
+/*laugh*/
+
+var myName; // declaring a variable
+
+
+/* Storing Values with the Assignment Operator */
+var a = 7; 
+
+
+/* Initializing Variables with the Assignment Operator */
+var myVar = a;
+var a = 9;
+
+
+/* Initialize these three variables */
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+
+/*Uninitialized Variables*/
+a = a + 1; // 5 + 1 = 6
+b = b + 5; // 10 + 5 = 15
+c = c + " String!"; //"I am a" + "String!" = "I am a String!"
+// output: a = 6, b = 15, c = "I am a String!"
+
+
+/*Understanding Case Sensitivity in Variables*/
+
+// Declarations
+var studlyCapVar = 10;
+var properCamelCase = "A String";
+var titleCaseOver = 9000;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
