aecc39527c9f3cd4643bf533ec93b28e9490a88d
Jackie <JGtheophila19@gmail.com>
2018-11-08 11:43:15 -0500

    Create golf.js

---
+++ Other/golf.js
@@ -0,0 +1,26 @@
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  
+  
+  return "Change Me";
+  // Only change code above this line
+}
+
+// Change these values to test
+golfScore(5, 4);
+
+
+// each hole = par
+// strokes = average num expected to make
+// above or below par your strokes = different nickname
+// par an strokes will be positive numeric
+
+// Strokes	        Return
+// 1	            "Hole-in-one!"
+// <= par - 2	    "Eagle"
+// par - 1	      "Birdie"
+// par	          "Par"
+// par + 1	      "Bogey"
+// par + 2	      "Double Bogey"
+// >= par + 3	    "Go Home!"
