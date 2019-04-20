5770577253cd081df02abcf134bc4833e44195e4
Jackie <JGtheophila19@gmail.com>
2019-04-19 23:47:59 -0400

    Find the Nth-to-Last Character in a String

---
+++ Bracket/4-bracket.js
@@ -0,0 +1,11 @@
+// can use the same principle to retrieve the last character in a string to retrieve the Nth-to-last character.
+
+var lastName = "Lovelace";
+
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // c
+
+/*
+        string = L  o  v  e  l  a  c  e
+    zero index = 0  1  2  3  4  5  6  7
+     subtract = -8 -7 -6 -5 -4 -3 -2 -1
+*/
