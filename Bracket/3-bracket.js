0b1dd6ea2361d3398262bb1e24f59111135608ae
Jackie <JGtheophila19@gmail.com>
2019-04-19 23:44:43 -0400

    Find the Last Character in a String

---
+++ Bracket/3-bracket.js
@@ -0,0 +1,11 @@
+// can also subtract one from the string's length in order to get the last letter of a string.
+
+var lastName = "Lovelace";
+
+var lastLetterOfLastName = lastName[lastName.length - 1]; // 'e'
+
+/*
+        string = L  o  v  e  l  a  c  e
+    zero index = 0  1  2  3  4  5  6  7
+     subtract = -8 -7 -6 -5 -4 -3 -2 -1
+*/
