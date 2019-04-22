2bb44436b24d08e5d2129c44b2d106091903d614
Jackie <JGtheophila19@gmail.com>
2019-04-21 23:29:38 -0400

    Manipulate Arrays With push()

---
+++ Array/arrayPush.js
@@ -0,0 +1,7 @@
+/* To append data to the end of an array using the push() function that 
+takes one or more parameters and "pushes" them onto the end of the array */
+
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+
+// [["John", 23], ["cat", 2], ["dog", 3]]
