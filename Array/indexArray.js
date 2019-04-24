647fedb8f029aec0ab26443a1ec5621fa9a16050
Jackie <JGtheophila19@gmail.com>
2019-04-24 19:52:46 -0400

    Access Array Data with Indexes

---
+++ Array/indexArray.js
@@ -0,0 +1,10 @@
+/* Can access the data inside arrays using indexes.
+Array indexes are written in the same bracket notation that strings use, 
+except that instead of specifying a character, they are specifying an entry in the array. 
+Arrays use zero-based indexing, so the first element in an array is element 0. */
+
+var array = [50,60,70]; 
+
+array[0]; // equals 50
+
+var data = array[1]; // equals 60
