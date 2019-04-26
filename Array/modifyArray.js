81fb1433a1c376880412af7040e6fb602f7d67a3
Jackie <JGtheophila19@gmail.com>
2019-04-25 22:14:25 -0400

    Modify Array Data With Indexes

---
+++ Array/modifyArray.js
@@ -0,0 +1,9 @@
+// the entries of arrays are mutable and can be changed freely
+
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+// ourArray now equals [18,45,99].
+
+// Modifying the data stored at index 0 of myArray to a value of 45
+var myArray = [18,64,99];
+myArray[0] = 45;
